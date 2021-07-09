072e27aaa997f3567f1da2e6d1993a77fd22b47b
Dasha <dariianikolaieva8@gmail.com>
2021-07-09 13:52:55 +0300

    Adaptive design

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 
-
 let btn = document.querySelector('.downIconImage')
 let viewSection = document.getElementById('box')
 
 function scroll() {
-    debugger;
     viewSection.scrollIntoView({block:"center" , behavior: "smooth"})
 }
 
